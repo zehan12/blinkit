0319e51d89c29e691ca4522d0ecae1bb25258bce
zehan12 <zehan9211@gmail.com>
2025-01-02 22:35:51 +0530

    fix: :bug: send proper response on home route

--- api_gateway/lib/api_gateway_web/controllers/home_controller.ex
+++ api_gateway/lib/api_gateway_web/controllers/home_controller.ex
@@ -2,7 +2,7 @@ defmodule ApiGatewayWeb.HomeController do
   use ApiGatewayWeb, :controller
 
   def index(conn, _params) do
-    response = :api_gateway_is_working
+    response = %{message: "api_gateway_is_working"}
     json(conn, response)
   end
 end
