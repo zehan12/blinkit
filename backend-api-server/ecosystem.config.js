18b0702fb260e23308530cffb66fff26fe445351
zehan12 <zehan9211@gmail.com>
2025-01-02 23:49:58 +0530

    feat :whale: add docker

--- backend-api-server/ecosystem.config.js
+++ backend-api-server/ecosystem.config.js
@@ -1,11 +1,8 @@
-// ecosystem.config.js
 module.exports = {
     apps: [
         {
             name: "backend-api-server-1",
             script: "./dist/server.js",
-            watch: true, // PM2 watch option set to true
-            ignore_watch:["./logs"],
             instances: 1, // Number of instances (adjust as needed)
             autorestart: true,
             max_memory_restart: "1G",
@@ -19,15 +16,10 @@ module.exports = {
                 NODE_ENV: "production",
                 // Add other production-specific environment variables here
             },
-            // output: "./logs/backend-api-server-1-out.log",
-            // error: "./logs/backend-api-server-1-error.log",
-            // log_date_format: "YYYY-MM-DD HH:mm:ss",
         },
         {
             name: "backend-api-server-2",
             script: "./dist/server.js",
-            watch: true,
-            ignore_watch:["./logs"],
             instances: 1,
             autorestart: true,
             max_memory_restart: "1G",
@@ -39,15 +31,10 @@ module.exports = {
                 PORT: 3002,
                 NODE_ENV: "production",
             },
-            // output: "./logs/backend-api-server-2-out.log",
-            // error: "./logs/backend-api-server-2-error.log",
-            // log_date_format: "YYYY-MM-DD HH:mm:ss",
         },
         {
             name: "backend-api-server-3",
             script: "./dist/server.js",
-            watch: true,
-            ignore_watch:["./logs"],
             instances: 1,
             autorestart: true,
             max_memory_restart: "1G",
@@ -59,9 +46,6 @@ module.exports = {
                 PORT: 3003,
                 NODE_ENV: "production",
             },
-            // output: "./logs/backend-api-server-3-out.log",
-            // error: "./logs/backend-api-server-3-error.log",
-            // log_date_format: "YYYY-MM-DD HH:mm:ss",
         },
     ],
 };
