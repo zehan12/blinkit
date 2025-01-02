fc813e3d89fba896e2d25f702a1c6734d69d5ec1
zehan12 <zehan9211@gmail.com>
2025-01-02 12:56:00 +0530

    feat: :sparkles: create index api  in home controller with atom response

--- api_gateway/lib/api_gateway_web/router.ex
+++ api_gateway/lib/api_gateway_web/router.ex
@@ -2,27 +2,15 @@ defmodule ApiGatewayWeb.Router do
   use ApiGatewayWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
+  end
+
+  scope "/", ApiGatewayWeb do
+    pipe_through(:api)
+    get("/", HomeController, :index)
   end
 
   scope "/api", ApiGatewayWeb do
-    pipe_through :api
-  end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:api_gateway, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: ApiGatewayWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
+    pipe_through(:api)
   end
 end
