1cc6b2e0eeb1a599056df8d1cbca42ef6648eaf4
zehan12 <zehan9211@gmail.com>
2025-01-02 12:08:45 +0530

    feat: :package: add required dependencies auth, jsonwebtoken and port forword

--- api_gateway/mix.exs
+++ api_gateway/mix.exs
@@ -41,7 +41,10 @@ defmodule ApiGateway.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:plug_cowboy, "~> 2.5"},
+      {:joken, "~> 2.5"},
+      {:httpoison, "~> 1.8"}
     ]
   end
 
