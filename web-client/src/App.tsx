173f13e46eebf034ce90620ea7e9d6c36d28b6e0
zehan12 <zehan9211@gmail.com>
2025-01-03 19:54:07 +0530

    style: :art: integrated tailwind css

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -4,8 +4,9 @@ import './App.css'
 function App() {
   return (
     <Fragment>
-      <main>
-        <h1>ONLINE DELIVERY APP</h1>
+      <main className='w-full h-screen bg-purple-400'>
+        <h1 className="text-3xl font-bold underline text-center text-red-500 bg-green-600 w-40 mx-auto">
+          Blinkit</h1>
       </main>
     </Fragment>
   )
